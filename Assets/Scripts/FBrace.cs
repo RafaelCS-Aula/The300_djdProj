5d612248db82d443c38c835dbed74d2363076d8f
BananaCat <joana.f.silva.99@gmail.com>
2019-05-27 00:58:20 +0100

    Enemies, EnemySpawner, Formations(Brace, Charge, Idle & Cover)

---
+++ Assets/Scripts/FBrace.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Standart defensive formation, helps army hold their ground and moves slower.
+/// </summary>
+public class FBrace : Formation
+{
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        thisFormation = EnumArmyStates.Brace;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (playerControl)
+        {
+
+            if(Input.GetAxis("Brace") > 0)
+            {
+                ActivateFormation();
+                Debug.Log("Brace");
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/FCover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  Most defensive position possible, guards agains't projectiles.!--
+/// But can't move while on it.
+/// </summary>
+public class FCover : Formation
+{
+
+   
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        thisFormation = EnumArmyStates.Cover;        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        if (playerControl)
+        {
+
+            if(Input.GetAxis("Cover") > 0)
+            {
+                ActivateFormation();
+            }
+
+        }
+
+    }
+}
