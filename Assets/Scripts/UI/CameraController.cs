128e7c028e6fe95445e74479e17d411a6d04f074
inesbarradas <inesmartinsbarradas1996@gmail.com>
2019-06-17 07:07:36 +0100

    Adição dos scripts CameraController, MainMenu ,AnimationControl, MouseTarget, Paralax, ParalaxObjs

---
+++ Assets/Scripts/UI/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Controls the camera relative to the player
+/// </summary>
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float lookAhead;
+
+    [SerializeField]
+    private float xMax;
+
+    [SerializeField]
+    private float yMax;
+
+    [SerializeField]
+    private float xMin;
+
+    [SerializeField]
+    private float yMin;
+
+    public Transform target;
+
+    private void Start()
+    {
+        target = GameObject.Find("Player").transform;
+        return;
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp((target.position.x + lookAhead), xMin, xMax), Mathf.Clamp((target.position.y - lookAhead), yMin, yMax), transform.position.z);
+
+    }
+}
